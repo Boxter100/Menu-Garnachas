201a55e0cb57f86b08a5c8f084d473a34243d7ca
Boxter100 <jared.nicolas779@gmail.com>
2024-11-07 18:25:39 -0600

    2.0

--- Transferencia/script.js
+++ Transferencia/script.js
@@ -1,7 +1,11 @@
+// Función para copiar al portapapeles usando 'data-copy'
 function copyToClipboard(id) {
     // Obtener el elemento por su ID
-    const textToCopy = document.getElementById(id).innerText;
-
+    const element = document.getElementById(id);
+    
+    // Obtener el valor del atributo 'data-copy'
+    const textToCopy = element.getAttribute('data-copy');
+    
     // Usar la API de portapapeles para copiar el texto
     navigator.clipboard.writeText(textToCopy)
         .then(() => {
